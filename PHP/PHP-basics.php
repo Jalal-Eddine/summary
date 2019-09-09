8e1a9dad3bab01d54f0403b847f990a7638930f5
Jalal-Eddine El-Habbazi <Jalaleddine.elhabbazi@gmail.com>
2019-09-09 12:11:35 +0200

    update

---
+++ PHP/PHP-basics.php
@@ -0,0 +1,23 @@
+<?php /*******/ ?>
+<?php include("fichier.php") ?>
+
+<!-- LES VARIABLES -->
+<!--  ************ -->
+<?php 
+$NomDe = "bakaka";
+$number = 55;
+$binair = true;
+echo "this man is number years old";
+echo 'this man is ' . number . 'years old';
+ ?>
+<!-- CONDITIONS -->
+<!-- *********** -->
+<?php 
+==//est égal à
+> //Est supérieur à
+<//Est inférieur à
+>=//Est supérieur ou égal à
+<=//Est inférieur ou égal à
+!=//Est différent de
+AND && //et
+OR || // ou
